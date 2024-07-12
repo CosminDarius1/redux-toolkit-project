75092ee8676718f2a1d6424f6229d7c5d6b88882
Cosmin Sas <cosmindariussas@gmail.com>
2024-07-12 16:50:33 +0100

    successfully created the modal feature for removing cart items

--- src/Components/CartContainer.jsx
+++ src/Components/CartContainer.jsx
@@ -1,6 +1,6 @@
-import { clearCart } from "../features/cart/cartSlice";
 import CartItem from "./CartItem";
 import {useDispatch, useSelector } from "react-redux";
+import { openModal } from "../features/modal/modalSlice";
 
 
 const CartContainer = () => {
@@ -30,9 +30,9 @@ const CartContainer = () => {
         <footer>
             <hr/>
             <div className="cart-total">
-                <h4>total<span>${total}</span></h4>
+                <h4>total<span>${total.toFixed(2)}</span></h4>
             </div>
-            <button className="btn clear-btn" onClick={() => dispatch(clearCart())}>clear cart</button>
+            <button className="btn clear-btn" onClick={() => dispatch(openModal())}>clear cart</button>
         </footer>
     </section>
 }
